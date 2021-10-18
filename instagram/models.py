d60684c43462fd5ad02a06cbe4ea48ae4ff72a07
kilonzijnr <vkilonzi05@gmail.com>
2021-10-18 09:58:43 +0300

    created a model class for Image post

--- instagram/models.py
+++ instagram/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
@@ -22,6 +23,16 @@ class Likes(models.Model):
 
     likes = models.IntegerField(default=0)
 
+class Image(models.Model):
+    """Model for handling Image posts by users"""
 
+    image = models.ImageField()
+    name = models.CharField(max_length= 25)
+    caption = models.CharField(max_length= 100)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, default= None)
+    likes = models.ForeignKey(Likes, on_delete=CASCADE, default=None)
+    comments = models.CharField(max_length= 120)
+    post_time = models.DateTimeField(auto_now_add= True)
+    
 
 
