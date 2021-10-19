2d95b35935538987875691382ecf11a96e4ab8ad
kilonzijnr <vkilonzi05@gmail.com>
2021-10-19 12:59:01 +0300

    imported several resources to views files

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from instagram.models import *
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your views here.
+
