bac33a48989f28ef4c0710cfd552289d6ff2f91e
kilonzijnr <vkilonzi05@gmail.com>
2021-10-19 12:08:16 +0300

    created more methods for Profile Model

--- instagram/models.py
+++ instagram/models.py
@@ -1,22 +1,73 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Profile(models.Model):
     """Model for handling User Profile"""
 
-    photo = models.ImageField()
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    username = models.CharField(max_length = 25)
+    signup_date = models.DateTimeField(auto_now_add= True)
+    profile_photo = models.ImageField()
+    followers = models.ManyToManyField(User, related_name='followers', blank= True)
     bio = models.CharField(max_length= 70)
 
     def __str__(self):
         return self.name
 
+    def total_followers(self):
+        """Method to return total numberof followers"""
+        return self.followers.count()
+
     def save_profile(self):
+        """Method to save profile to the database"""
         self.save()
 
     def delete_profile(self):
+        """Method to delete profile from the database"""
         self.delete()
+
+    def update_profile(self,new):
+        """Method to update user profile
+        
+        Args:
+            new([type]): [description]
+
+        """
+        self.username = new.username
+        self.bio = new.bio
+        self.profile_photo = new.profile_pic
+        self.save()
+
+    @classmethod 
+    def get_following(cls,user):
+        """Method to return all users a specific user is following """
+        following = user.followers.all()
+        users = []
+        for profile in following:
+            user = User.objects.get(profile = profile)
+            users.append(user)
+
+        return users
+
+    @classmethod 
+    def search_profile(cls,search_term):
+        """Method to return profiles with a provided search term
+        
+        Args:
+            search_term([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        profiles = cls.objects.filter(username_icontains = search_term)
+
+        return profiles
+
+
+
 
 class Likes(models.Model):
     """Model for handling Image likes"""
